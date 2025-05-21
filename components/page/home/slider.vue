aa60af381c8f028ecc52981073c5061729b95f3b
nmhung <hung.hung24042001@gmail.com>
2025-05-22 02:29:16 +0700

    Update slider.vue

--- components/page/home/slider.vue
+++ components/page/home/slider.vue
@@ -1,55 +1,55 @@
 <script setup>
 const slides = ref(Array.from({ length: 9 }));
-onMounted(() => {});
+onMounted(() => { });
 </script>
 
 <template>
-  <ClientOnly>
-    <swiper-container
-
-      :pagination="{
+    <ClientOnly>
+        <swiper-container :autoplay="{
+            delay: 4000,
+            disableOnInteraction: false,
+        }" :pagination="{
         clickable: true,
-      }"
-    >
-      <swiper-slide v-for="(slide, idx) in slides" :key="idx">
-        <div class="item__slide">
-            <nuxt-link to="/"
-          ><img src="/images/slider/item1.png" alt="img-slide"
-        /></nuxt-link>
-        </div>
-      </swiper-slide>
-    </swiper-container>
-  </ClientOnly>
+    }">
+            <swiper-slide v-for="(slide, idx) in slides" :key="idx">
+                <div class="item__slide">
+                    <nuxt-link to="/"><img src="/images/slider/item1.png" alt="img-slide" /></nuxt-link>
+                </div>
+            </swiper-slide>
+        </swiper-container>
+    </ClientOnly>
 </template>
 
 <style lang="scss" scoped>
 swiper-slide {
-display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: bold;
-  height: 28.125vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+    height: 28.125vw;
 }
 
 swiper-container::part(bullet),
 swiper-container::part(bullet-active) {
-  width: 2.8rem;
-  height: 0.4rem;
-  border-radius: 2rem;
-  background-color: $bg-primary;
-  opacity: 1;
+    width: 2.8rem;
+    height: 0.4rem;
+    border-radius: 2rem;
+    background-color: $bg-primary;
+    opacity: 1;
 }
 
 swiper-container::part(bullet-active) {
-  background-color: $bg-active;
+    background-color: $bg-active;
 }
-.item__slide{
+
+.item__slide {
     width: 100%;
-    a{
+
+    a {
         display: inline-block;
         width: 100%;
 
-        > img{
+        >img {
             width: 100%;
             // height: 100%;
             margin-bottom: -100px;
