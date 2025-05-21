5f214d34a6453f2009b3ab7392f3bd687ee7c4df
nmhung <hung.hung24042001@gmail.com>
2025-05-22 01:58:27 +0700

    add sliide

--- components/page/home/slider.vue
+++ components/page/home/slider.vue
@@ -1,31 +1,44 @@
 <script setup>
-// Create 10 slides
-const slides = ref(Array.from({ length: 9 }))
-onMounted(() => {
-})
+const slides = ref(Array.from({ length: 9 }));
+onMounted(() => {});
 </script>
 
 <template>
   <ClientOnly>
     <swiper-container
-
-    :pagination="{
-      clickable: true,
-    }"
+      :autoplay="{
+        delay: 4000,
+        disableOnInteraction: false,
+      }"
+      :pagination="{
+        clickable: true,
+      }"
     >
-      <swiper-slide
-        v-for="(slide, idx) in slides"
-        :key="idx"
-      >
-        <nuxt-link to="/"><img src="/images/slider/item1.png" alt="img-slide"></nuxt-link>
+      <swiper-slide v-for="(slide, idx) in slides" :key="idx">
+        <nuxt-link to="/"
+          ><img src="/images/slider/item1.png" alt="img-slide"
+        /></nuxt-link>
       </swiper-slide>
     </swiper-container>
   </ClientOnly>
 </template>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 swiper-slide {
   height: 28.125vw;
   min-height: 200px;
+}
+
+swiper-container::part(bullet),
+swiper-container::part(bullet-active) {
+  width: 2.8rem;
+  height: 0.4rem;
+  border-radius: 2rem;
+  background-color: $bg-primary;
+  opacity: 1;
+}
+
+swiper-container::part(bullet-active) {
+  background-color: $bg-active;
 }
 </style>
