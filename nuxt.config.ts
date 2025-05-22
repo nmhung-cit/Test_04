805900911fbd4fae47211ae9f1ce869fb1613f0b
nmhung <hung.hung24042001@gmail.com>
2025-05-23 05:15:49 +0700

    test deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['@/assets/scss/main.scss'],
-
+  ssr: true,
+  app: {
+    baseURL: 'Test_04', 
+  },
   vite: {
     css: {
       preprocessorOptions: {
